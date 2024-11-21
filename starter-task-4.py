4adfc16183b3c1dcd8fe16d613c31ca4dba5d65d
Yahya833 <yahya_alalawi@biswashington.org>
2024-11-21 17:38:30 +0000

    giysvdiuhdshusrgvbugvfsdugobfsvugisdvfigusdvuivsdugiodsvfugiswefd

--- starter-task-4.py
+++ starter-task-4.py
@@ -3,18 +3,18 @@ score = int(input("Enter your score: "))
 
 # if the score is 90 or above
 if score >= 90:
-    # TODO: Assign the grade "A" to a variable
+    grade = "a"
 
 # if the score is between 80 and 89
 elif score >= 80:
-    # TODO: Assign the grade "B" to a variable
+    grade = "b"
 
 # if the score is between 70 and 79
 elif score >= 70:
-    # TODO: Assign the grade "C" to a variable
+    grade = "c"
 
 # if the score is below 70
 else:
-    # TODO: Assign the grade "Fail" to a variable
+    grade = "fail!!!!!"
 
-# TODO: Print the grade variable
+print(f"You got a ",grade)
--- starter-task-5.py
+++ starter-task-5.py
@@ -4,18 +4,19 @@ secret_number = 7
 # Ask the user to guess the secret number
 guess = int(input("Guess the secret number: "))
 
-# if the guess is correct
-if guess == secret_number:
-    # TODO: Print a message indicating the guess is correct
+while guess != secret_number:
+    #if the guess is too high
+    if guess > secret_number:
+        print("too hig")
+        guess = int(input("Try again: "))
 
-# if the guess is too high
-elif guess > secret_number:
-    # TODO: Print a message saying the guess is too high
-    # TODO: Ask the user to guess again and update the guess variable
+    # if the guess is too low
+    else:
+        print("too low")
+        guess = int(input("Try agian: "))
 
-# if the guess is too low
-else:
-    # TODO: Print a message saying the guess is too low
-    # TODO: Ask the user to guess again and update the guess variable
 
-# TODO: Continue asking until the user guesses correctly (use a loop)
+
+
+ # if the guess is correct
+print("good job")
